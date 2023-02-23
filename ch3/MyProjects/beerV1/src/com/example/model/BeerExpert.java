d44d3a8f39199374bf1fc7c3b0be2ef05fb5d1aa
romuloabbiati <rhabbiati@hotmail.com>
2023-02-23 04:50:55 +0000

    model class BeerExpert.java and .class

--- ch3/MyProjects/beerV1/src/com/example/model/BeerExpert.java
+++ ch3/MyProjects/beerV1/src/com/example/model/BeerExpert.java
@@ -1,1 +1,18 @@
-java class - model
+package com.example.model;
+import java.util.*;
+
+public class BeerExpert {
+
+	public List<String> getBrands(String colour) {
+		List<String> brands = new ArrayList<>();
+		if(colour.equals("amber")) {
+			brands.add("Jack Amber");
+			brands.add("Red Moose");
+		} else {
+			brands.add("Jail Pale Ale");
+			brands.add("Gout Stout");
+		}
+		return (brands);
+	}
+
+}
